a6fd69c71473f3d5e93b2a1d0bdce4c5c5311198
ITSME2256 <supakorn.ch@ksu.ac.th>
2025-03-12 14:29:01 +0700

    สีปุ่มออกงาน

--- time-tracker/src/components/DashboardPage.js
+++ time-tracker/src/components/DashboardPage.js
@@ -313,7 +313,7 @@ const DashboardPage = () => {
             {/* ปุ่มออกงาน */}
             <Button
               variant="contained"
-              color="primary"
+              color="warning"
               fullWidth
               disabled={!isLogoutEnabled || isOutOfRange}
               onClick={handleLogout}
